90683678ca32a29c36c9625dbdb9ac815828a42a
Beatriz Asuncion <111472853+beaasuncion@users.noreply.github.com>
2023-05-11 10:30:25 +0800

    Add files via upload

---
+++ exercise3/Dog/Pomsky.java
@@ -0,0 +1,9 @@
+class Pomsky extends Husky implements Pomeranian {
+
+   
+    public String height = "10 inches";
+
+    public void bark() {
+        System.out.println("My pomsky is barking");
+    }
+}
